d3d4d5daaf238c0f24c111e4a8d6035e66a8aedf
Mohammad Al Hammod <m-khaled89@hotmail.com>
2019-08-13 11:26:53 -0500

    Add project files.

---
+++ EcommerceDbFirst/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EcommerceDbFirst.MvcApplication" Language="C#" %>
---
+++ EcommerceDbFirst/Models/CustomerManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcommerceDbFirst.Models
+{
+    public class CustomerManager
+    {
+        ECommerceEntities db = new ECommerceEntities();
+
+        public IList<Customer> GetCustomersInCity(string city)
+        {
+            var result = db.Customers.Where(c => c.City == city).ToList();
+            return result;
+        }
+    }
+}
